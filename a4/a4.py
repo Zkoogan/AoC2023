dc2ef98f7342ef138311baf1ef1a010756bbe290
Zkoogan <andre.skoog.jobb@gmail.com>
2023-12-04 17:17:55 +0100

    A4 solution

---
+++ a4/a4.py
@@ -0,0 +1,24 @@
+import numpy as np
+import re
+import math
+
+with open('a4.txt', 'r') as f:
+    data =  [x.split(':')[1].replace('\n', '').strip() for x in f.readlines()]
+
+data = [(x.split('|')[0].strip(), x.split('|')[1].strip()) for x in data]
+data = [(re.split('\s+', x[0]), re.split('\s+', x[1])) for x in data]
+
+data = [list(set(x[0]) & set(x[1])) for x in data]
+
+score = int(np.sum([math.pow(2, len(x))//2 for x in data]))
+
+print(score)
+
+numScratchCards = np.ones(len(data))
+
+print(data)
+
+for index, matches in enumerate(data):
+    numScratchCards[index+1:index+len(matches)+1] += numScratchCards[index]
+
+print(int(np.sum(numScratchCards)))
